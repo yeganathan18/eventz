fd2160bda061e492196103d8454ced069462ec45
Yeganathan S <hilariousengineer@protonmail.com>
2023-02-06 18:27:08 +0530

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
+import cx from "classnames";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <main className={cx(inter.variable, "font-sans")}>
+        <Component {...pageProps} />
+      </main>
+    </SessionContextProvider>
+  );
 }
+export default MyApp;
